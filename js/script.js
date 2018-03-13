ff25e358b172db4558a49a1628b26e43b45219dd
Jona535h <jona535h@edu.easj.dk>
2018-03-13 17:26:08 +0100

    Header og navigation færdig

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
+
+
+
 $(document).ready (function(){
+
+
+
+    //jQuery til at åbne burgermenu
+    $(".overlayOpen").click(function () {
+        $(".overlayMenu").show('100');
+    });
+    //jQuery til at lukke burgermenu
+    $(".overlayClose").click(function () {
+        $(".overlayMenu").hide('100');
+    });
+
+
+
 
 });
